9a41865cb0904310186802380fc21d08d214f984
veip007 <veip007@vip.qq.com>
2018-11-18 20:19:21 +0800

    # 优化进程检测机制，避免部分系统检测进程状态错误的问题

--- aria2.sh
+++ aria2.sh
@@ -19,6 +19,7 @@ Folder="/usr/local/aria2"
 aria2c="/usr/bin/aria2c"
 Crontab_file="/usr/bin/crontab"
 
+
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
